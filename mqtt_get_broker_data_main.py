beaffd3adf35a582f78a5705861a9fbf9060be63
Kalevits <kalevisworking@gmail.com>
2017-08-10 09:53:05 +0300

    Add files via upload

---
+++ mqtt_get_broker_data_main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import paho.mqtt.client as mqtt #import the client1
+import time
+import mqtt_save_broker_values_to_db
+import mqtt_send_garden_data
+
+############
+def on_message(client, userdata, message):
+    print("message received " ,str(message.payload.decode("utf-8")))
+    print("message topic=",message.topic)
+    print("message qos=",message.qos)
+    print("message retain flag=",message.retain)
+    msg = str(message.payload.decode("utf-8"))
+    # Save data to mysql database
+    save_result = mqtt_save_broker_values_to_db.save_to_db(msg)
+    print(save_result)
+    # Send data to thingspeak
+    send_result = mqtt_send_garden_data.send_to_thingspeak(msg)
+    print(send_result)
+    
+############
+def main():
+    broker_address="localhost"
+
+    print("creating new instance")
+    client = mqtt.Client("P1") #create new instance
+    client.on_message=on_message #attach function to callback
+    print("connecting to broker")
+    client.connect(broker_address) #connect to broker
+    #client.loop_start() #start the loop
+    print("Subscribing to topic","sensors/test/temperature")
+    client.subscribe("sensors/test/temperature")
+    time.sleep(10) # wait
+    client.loop_forever() #start the loop
+
+main()
--- mqtt_save_broker_values_to_db.py
+++ mqtt_save_broker_values_to_db.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 
-import paho.mqtt.client as mqtt #import the client1
-import time
 import mysql.connector
-import mqtt_send_garden_data
 
 def save_to_db(msg):
-    conn = mysql.connector.connect(user='<username>', password='<password>', database='<databasename>')
+    conn = mysql.connector.connect(user='<user>', password='<password>', database='<database name>')
     cursor = conn.cursor()
     new_conditions = ('INSERT INTO olosuhteet '
         '(temperature, humidity, temp_20cm, soil_temperature, soil_humidity, gh_temperature,'
@@ -66,31 +63,3 @@ def save_to_db(msg):
         return('Data values added!')
     cursor.close()
     conn.close()
-
-############
-def on_message(client, userdata, message):
-    print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
-    print("message qos=",message.qos)
-    print("message retain flag=",message.retain)
-    msg = str(message.payload.decode("utf-8"))
-    # Save data to mysql database
-    save_result = save_to_db(msg)
-    print(save_result)
-    # Send data to thingspeak
-    send_result = mqtt_send_garden_data.send_to_thingspeak(msg)
-    print(send_result)
-    
-############
-broker_address="localhost"
-
-print("creating new instance")
-client = mqtt.Client("P1") #create new instance
-client.on_message=on_message #attach function to callback
-print("connecting to broker")
-client.connect(broker_address) #connect to broker
-#client.loop_start() #start the loop
-print("Subscribing to topic","sensors/test/temperature")
-client.subscribe("sensors/test/temperature")
-time.sleep(10) # wait
-client.loop_forever() #start the loop
